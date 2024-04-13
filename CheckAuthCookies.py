bc31bce056ba6efc1b532d086ff4d3295141d495
Noah Policke <noah@policke.com>
2024-04-13 17:21:40 +0200

    Fix alot of issues

--- CheckAuthCookies.py
+++ CheckAuthCookies.py
@@ -1,8 +1,9 @@
 import requests
 import sys
 import subprocess
+import ipaddress
 
-#Hilfsfunktion fürs parsen der NMAP Antwort
+#Parse NMAP output
 def parse_nmap_output(output):
     open_ports = []
     lines = output.split('\n')
@@ -12,6 +13,7 @@ def parse_nmap_output(output):
             open_ports.append(port)
     return open_ports
 
+#Parse Dirb output
 def parse_dirb_output(output):
     admin_pages = []
     lines = output.split('\n')
@@ -25,58 +27,67 @@ def parse_dirb_output(output):
     return admin_pages
 
 
+#Split IPs in IP-Range
+def split_ip_range(ip_range):
+    start_ip, end_ip = ip_range.split('-')
+    start_ip_parts = start_ip.split('.')
+    end_ip_parts = end_ip.split('.')
+    
+    ips = []
+    for i in range(int(start_ip_parts[-1]), int(end_ip_parts[-1]) + 1):
+        ip = ".".join(start_ip_parts[:-1] + [str(i)])
+        ips.append(ipaddress.IPv4Address(ip))
+    
+    return ips
 
 #Scan IP and Portrange with NMAP
 def scan_ip_range(ip_range, port_range="80,443"):
-    command = ["nmap", "-p", port_range, ip_range]
-    try:
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
-        if "Host seems down" in output:
-            print("Die IP-Adresse oder das Netzwerk ist nicht erreichbar:", ip_range)
+    ips = split_ip_range(ip_range)
+    for ip in ips:
+        command = ["nmap", "-p", port_range, str(ip)]
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            if "Host seems down" in output:
+                print("Die IP-Adresse oder das Netzwerk ist nicht erreichbar:", ip)
+            else:
+                print("Ein oder mehrere offene Ports gefunden in der IP:", ip)
+                print(output)
+                print('\n --------------------------------------------------------------- \n')
+                open_ports = parse_nmap_output(output)
+                for port in open_ports:
+                    if port == "80":
+                        url = f"http://{ip}"
+                        find_admin_pages(url)
+                        print('\n --------------------------------------------------------------- \n')
 
-        else:
-            print("Ein oder mehrere offene Ports gefunden in der IP-Range:", ip_range)
-            print("Ausgabe von Nmap:")
-            #print(output)
-            open_ports = parse_nmap_output(output)
-            for port in open_ports:
-                if port == "80":
-                    url = f"http://{ip_range}"
-                    print("URL - NMAP: ", url)
-                    find_admin_pages(url)
-                    print('\n --------------------------------------------------------------- \n')
-
-                elif port == "443":
-                    url = f"https://{ip_range}"
-                    print("URL - NMAP: ", url)
-                    find_admin_pages(url)
-                    print('\n --------------------------------------------------------------- \n')
+                    elif port == "443":
+                        url = f"https://{ip}"
+                        find_admin_pages(url)
+                        print('\n --------------------------------------------------------------- \n')
                   
-                else:
-                    url = f"https://{ip_range}:{port}"
-                    print("URL - NMAP: ", url)
-                    find_admin_pages(url)
-                    print('\n --------------------------------------------------------------- \n')
-    except subprocess.CalledProcessError as e:
-        print("Fehler bei der Ausführung von Nmap:", e)
+                    else:
+                        url = f"https://{ip}:{port}"
+                        find_admin_pages(url)
+                        print('\n --------------------------------------------------------------- \n')
+        except subprocess.CalledProcessError as e:
+            print("Fehler bei der Ausführung von Nmap:", e)
 
 
 
 #search for admin/login pages
 def find_admin_pages(url):
-    command = ["dirb", url, "./dirb/adminpages.txt"] ############################################korrigieren
+    command = ["dirb", url, "./dirb/adminpages.txt"]
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
         if "FOUND: 0" in output:
-            print("URL - DIRB - no sites: ", url)
+            print("Es wurden keine Unterseiten gefunden für: ", url)
             check_set_cookie(url)
             check_authentication_methods(url)  
         else:
             admin_pages = parse_dirb_output(output)
             print(admin_pages)
             for admin_page in admin_pages:
-                #admin_url = f"{url}/{admin_page}"
-                print("URL - DIRB - Site Found: ", admin_page)
+                print("Folgende Seiten wurden gefunden: ", admin_page)
                 find_xss_vuln(admin_page)
                 check_set_cookie(admin_page)
                 check_authentication_methods(admin_page)
@@ -94,10 +105,10 @@ def find_xss_vuln(url):
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
         if "FOUND: 0" in output:
-            print("No XSS Vulnabilities found for: ", url)
+            print("Es wurden keine XSS Vulnabilities gefunden für: ", url)
 
         else:
-            print("XSS Vulnabilities found for: ", url) 
+            print("Es wurden folgende XSS Vulnabilities gefunden für: ", url) 
             print(output)
             
     except subprocess.CalledProcessError as e:
@@ -126,6 +137,11 @@ def check_authentication_methods(url):
     try:
         response = requests.get(url)
 
+        if "login" in response.text:
+            print("Formularbasierte Authentifizierung gefunden")
+        else:
+            print("Keine formularbasierte Authentifizierung gefunden")
+
         cookies = response.cookies
 
         session_cookie_found = False
@@ -139,7 +155,6 @@ def check_authentication_methods(url):
         else:
             print("Keine Session Cookies gefunden")
 
-        # Überprüfen, ob ein Authentifizierungs-Cookie vorhanden ist
         auth_cookie_found = False
         for cookie in cookies:
             if "auth" in cookie.name.lower():
@@ -152,13 +167,11 @@ def check_authentication_methods(url):
             print("Keine authentifizierungs Cookies gefunden")
 
 
-        # Überprüfen, ob die Website einen Formular-Login verwendet
-        if "login" in response.text:
-            print("Formularbasierte Authentifizierung gefunden!")
+
 
         # Auf redirects prüfen
         if response.history:
-            print("Die Webseite leitet nach dem Login um.")
+            print("Die Webseite leitet nach dem Login um")
 
 
     except requests.RequestException as e:
@@ -168,7 +181,9 @@ def check_authentication_methods(url):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print("Verwendung: python script.py <StartIP-EndIP> [Port-Range]")
+        print("Verwendung: python CheckAuthCookies.py [StartIP-EndIP/FQDN] [Port-Range/Ports (Kommagetrennt)]")
+        print("Beispiel: python CheckAuthCookies.py example.com 443,8443")
+        print("Beispiel: python CheckAuthCookies.py 192.168.100.1-5 443-8443")
         sys.exit(1)
     
     ip_range = sys.argv[1]
